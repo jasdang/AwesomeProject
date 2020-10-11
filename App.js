ed75468cd13d5d1f905a198bc6b03b30b5ef0263
Jasmine Dang <ngandth.1992@gmail.com>
2020-10-11 23:11:48 +0800

    added a button

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const handleClick = () => {
+    console.log('Clicked');
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Click me' onPress={handleClick}></Button>
       <StatusBar style="auto" />
     </View>
   );
